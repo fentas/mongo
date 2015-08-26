3a8399712359a66ea7bd78e3d85f1aa66e5311ae
fentas <jan.guth@gmail.com>
2015-08-26 10:49:51 +0200

    update script

---
+++ cluster/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT=`which git`
+if [ -z $GIT ]; then
+  apt-get update && \
+  apt-get install -y git
+fi
+
+rm -R /tmp/mongo
+git clone https://github.com/fentas/mongo.git /tmp
+rm -R /opt/*
+cp -R /tmp/mongo/cluster/* /opt
+
+forever stop all
+forever start /opt/index.js
